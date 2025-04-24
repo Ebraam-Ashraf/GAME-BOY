3d16e9d768e19b0d2a3b96c2c58c8b75176cdaff
Ebraam <168275494+Ibraam-Ashraf@users.noreply.github.com>
2025-04-24 22:38:54 +0200

    Update main.s

--- main.s
+++ main.s
@@ -1,1 +1,126 @@
 
+; Pin Connection
+; D0-7  =   PA0-7	Data BUS	    Put your command or data on this bus
+; RD    =   PA8		Read pin	    Read from touch screen input 
+; WR    =   PA9		Write pin	    Write data/command to display
+; RS    =   PA10	Command pin	    Choose command or data to write
+; CS    =   PA11	Chip Select	    Enable the TFT,(active low)
+; RST   =   PA12	Reset		    Reset the TFT (active low)
+; D0-7  =   PB0-4	5 Keys
+
+		
+    ;TFT
+    IMPORT GPIO_Init
+    IMPORT TFT_WriteCommand
+    IMPORT TFT_WriteData
+    IMPORT TFT_Init
+    IMPORT TFT_FillScreen
+    IMPORT Delay
+    IMPORT font_map
+    IMPORT Read_Keys
+    ;Graphics
+    IMPORT Draw_Pixel
+    IMPORT Draw_Char
+    IMPORT Draw_Msg
+    IMPORT Draw_Line
+    IMPORT Draw_Rectangle	
+    IMPORT Clear_Screen
+    IMPORT Init_Screen
+    
+	EXPORT __main
+		
+    AREA MYDATA, DATA, READWRITE    ; Declare the GameData section
+
+; ---------- COLORS ----------
+
+Red     EQU 0x001F   ; 00000 000000 11111
+Green   EQU 0x07E0   ; 00000 111111 00000
+Blue    EQU 0xF800   ; 11111 000000 00000
+Yellow  EQU 0x06FF   ; 11111 111111 00000
+White   EQU 0xFFFF   ; 11111 111111 11111
+Black   EQU 0x0000   ; 00000 000000 00000
+
+; PADDLE DATA
+
+PADDLE_WIDTH         DCW     10
+PADDLE_HEIGHT        DCW     40
+PADDLE_COLOR         DCW     3
+PADDLE_SPEED         DCW     2
+
+; BALL DATA
+BALL_X               DCW     170
+BALL_Y               DCW     110
+BALL_VELOCITY_X      DCW     2
+Ball_VELOCITY_Y      DCW     2
+BALL_COLOR           DCB     0
+
+; SCORE DATA AND LIVES
+SCORE                DCB     "SCORE: $"
+
+SCORE_COUNT_PLAYER_1 DCW     0
+SCORE_COUNT_PLAYER_2 DCW     0
+
+; STRING DATA
+string1              DCB     "ONE PLAYER MODE => 2.", 13, 10, "$"
+string2              DCB     "CO-OP PING PONG  => 4.", 13, 10, "$"
+string3              DCB     "Exit => Esc.", 13, 10, "$"
+
+; ESCAPE STATUS
+ESCSTATUS            DCB     0
+
+; TWO PLAYER MODE PADDLE DATA
+PADDLE_COLOR1        DCB     8
+PADDLE_COLOR2        DCB     8
+
+PADDLE_X1            DCW     50
+PADDLE_Y1            DCW     100
+
+PADDLE_X2            DCW     270
+PADDLE_Y2            DCW     100
+						
+
+            AREA    MYcode, CODE, READONLY
+		
+__main FUNCTION
+	
+    BL GPIO_Init
+
+    
+Options_page
+    ; Clear the screen colour
+    BL      TFT_FillScreen
+
+    ; Display option strings
+    MOV     R0, #10                ; Row (approximately 10 for the first string)
+    MOV     R1, #32                ; Column (approximately 32 for horizontal centering)
+    LDR     R2, =string1           ; Load address of first string
+    BL      Draw_Msg               ; Print string
+
+    MOV     R0, #12                ; Row (approximately 12 for the second string)
+    MOV     R1, #30                ; Column (approximately 30 for horizontal centering)
+    LDR     R2, =string2           ; Load address of second string
+    BL      Draw_Msg	           ; Print string
+
+    MOV     R0, #14                ; Row (approximately 14 for third string)
+    MOV     R1, #29                ; Column (approximately 29 for horizontal centering)
+    LDR     R2, =string3           ; Load address of third string
+    BL      Draw_Msg	           ; Print string
+
+    ; Wait for key press
+REENTER BL      Read_Keys           ; Call function to detect key press
+	
+	CMP R0,#1
+	BEQ ONE_PLAYER_MODE
+	
+	CMP R0,#2
+	BEQ PING_PONG_MODE
+	
+	CMP R0,#3
+	BEQ EXIT
+	
+	CMP R0,#4
+	BEQ REENTER
+	
+	ENDFUNC
+
+
